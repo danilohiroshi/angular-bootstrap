fabb9f5c05e5c4757b82bf72801050078f769490
Danilo Yamashita <danilohiroshi@gmail.com>
2018-05-25 11:06:27 -0300

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+
+import { $ } from 'jquery';
+import bootstrap from 'bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
